9e04db3b81f9091a6aa70152ed9371ace6861981
Sankalp Jhingran <sankalp.jhingran@salesforce.com>
2017-08-17 02:53:16 -0700

    remove port

--- server/models/index.js
+++ server/models/index.js
@@ -9,7 +9,7 @@ var DataTypes = require("sequelize");
   //Setting up the config
   var sequelize = new Sequelize('de5ml9a574vklj', 'eqyftdnqqogabd', 'd1f36b24ce55df93fa72221961b1a9f9328600fceaec3b081c223834a6a65b9b', {
     host: 'ec2-23-23-222-147.compute-1.amazonaws.com',
-    port: 5432,
+    //port: 5432,
     logging: console.log,
 });
 
