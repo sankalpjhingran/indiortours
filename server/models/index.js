b7df78423bf6ac744af23067b69dc82612f9cacb
Sankalp Jhingran <sankalp.jhingran@salesforce.com>
2017-08-18 15:50:16 -0700

    Update

--- server/models/index.js
+++ server/models/index.js
@@ -8,6 +8,8 @@ var DataTypes = require("sequelize");
 require('dotenv').config();
 
 var pe = process.env;
+console.log('====>6 ' + pe);
+console.log('====>6 ' + JSON.stringify(pe));
 const pguser = pe.PGUSER;
 const pgpass = pe.PGPASS;
 const pghost = pe.PGHOST;
@@ -18,8 +20,6 @@ console.log('====>2 ' + pgpass);
 console.log('====>3 ' + pghost);
 console.log('====>4 ' + pgport);
 console.log('====>5 ' + pgdatabase);
-console.log('====>6 ' + pe);
-console.log('====>6 ' + JSON.stringify(pe));
 console.log('====>6 ' + process.env.HEROKU_POSTGRESQL_BRONZE_URL);
 
 const defaultDbUrl = 'postgres://' + pguser + ':' + pgpass + '@' + pghost +
