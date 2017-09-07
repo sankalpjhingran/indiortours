92428e96b8fa51d8b219157f7a49fa4a78d86281
Sankalp Jhingran <sankalp.jhingran@salesforce.com>
2017-09-06 19:34:06 -0700

    Update

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path');
 //var favicon = require('serve-favicon');
+
+console.log('Initializing app.js file====>');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -21,6 +23,7 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(logger('production'));
 app.use(bodyParser.json());
